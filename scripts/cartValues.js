a5b2e2cdbe88ab4613af8f4af91c4f47dd9bd593
Mohamed Saheel <110540324+Md-saheel@users.noreply.github.com>
2024-10-24 15:37:10 +0530

    Add files via upload

--- scripts/cartValues.js
+++ scripts/cartValues.js
@@ -3,6 +3,7 @@ import { cartItems } from '../data/data.js';
 export default function cartValues() {
   document.querySelectorAll('.cart-value').forEach((valueElement) => {
     const { dessertId } = valueElement.dataset;
+
     cartItems.forEach((cartItem) => {
       if (cartItem.id === Number(dessertId)) {
         valueElement.value = cartItem.quantity;
