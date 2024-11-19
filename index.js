7b1e3e90615583c5b524b5b252c5aff2633f9319
Mohamed Saheel <110540324+Md-saheel@users.noreply.github.com>
2024-11-19 22:18:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import renderDesserts from './scripts/renderDesserts.js';
+import renderCart from './scripts/renderCart.js';
+import saveToStorage from './scripts/saveToStorage.js';
+import btnHandlers from './scripts/btn-handers.js';
+import cartValues from './scripts/cartValues.js';
+
+renderDesserts();
+renderCart();
+saveToStorage();
+btnHandlers();
+cartValues();
